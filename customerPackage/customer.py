b88bdf561f8df4bb0f1d3138677ec7addcee110d
maipants <tkrkh43@gmail.com>
2025-04-26 22:32:54 -0400

    Revert "Add files via upload"
    This reverts commit 093b5f1b5f3f4f881dfbf49678f8f789eaa6316c.

--- customerPackage/customer.py
+++ customerPackage/customer.py
@@ -7,16 +7,6 @@ class Customer(User):
     # adds, deletes, and modifies product and price to products
     # adds stock to warehouse
 
-
-    #Change added by Aradana
-    from cartPackage.shoppingcart import ShoppingCart  # import at top
-
-class Customer(User):
-    def __init__(self):
-        super().__init__("CUSTOMER")
-        self.shopping_cart = ShoppingCart()
-
-
     def __init__(self):
         super().__init__("CUSTOMER")
 
@@ -121,62 +111,3 @@ class Customer(User):
             self.commit() # commit transaction
         except: 
             return None
-
-    # Change by Aradana
-    def modifyAddress(self, address):
-    """
-    Modify address in customer info
-    """
-    try:
-        self.execute("""
-            UPDATE customers
-            SET address = %s
-            WHERE id = %s
-        """, (address,))
-        print(f"Address {address} successfully modified in table.")
-        self.commit()
-    except Exception as e:
-        print(f"Error modifying address: {e}")
-
-
-## Change by Aradana
-def customer_menu(customer):
-    catalog = ProductCatalog(customer)  # pass db connection!
-
-    while True:
-        print("\n--- Customer Menu ---")
-        print("1. Browse Products")
-        print("2. Add Product to Cart")
-        print("3. View Cart")
-        print("4. Checkout")
-        print("5. Manage Payment Cards")
-        print("6. Manage Addresses")
-        print("7. Logout")
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            catalog.browse_products()
-
-        elif choice == "2":
-            prod_id = input("Enter product ID to add: ")
-            qty = int(input("Enter quantity: "))
-            customer.shopping_cart.add_item(prod_id, qty)
-
-        elif choice == "3":
-            customer.shopping_cart.view_cart()
-
-        elif choice == "4":
-            customer.shopping_cart.checkout()
-
-        elif choice == "5":
-            print("Manage Payment Cards (to be implemented).")
-
-        elif choice == "6":
-            print("Manage Addresses (to be implemented).")
-
-        elif choice == "7":
-            print("Logging out...")
-            break
-
-        else:
-            print("Invalid choice. Try again.")
